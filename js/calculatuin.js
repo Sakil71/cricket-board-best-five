5113cb794af14e35a972b0ca106772e3c3d2e781
Shakil Ahamed <sakilahmed7810@gmail.com>
2022-08-22 19:05:25 +0600

    Fourth Commit

---
+++ js/calculatuin.js
@@ -0,0 +1,22 @@
+
+// Player expenses Calculate
+
+document.getElementById('calculate-button').addEventListener('click', function () {
+    const allPlayerList = document.querySelectorAll('#all-players li');
+    const allListLength = allPlayerList.length;
+    const inputElement = getInputFieldById('per-player-expenses');
+
+    const totalPlayerExpenses = allListLength * inputElement;
+    setElementById('player-expenses', totalPlayerExpenses);
+})
+
+// Calculate Total Expenses
+
+document.getElementById('total-calculate-button').addEventListener('click', function(){
+    const managerExpenses = getInputFieldById('manager-expense');
+    const coachExpenses = getInputFieldById('coach-expense');
+    const allPlayerExpense = getExpensesId('player-expenses');
+
+    const totalCost = managerExpenses + coachExpenses + allPlayerExpense;
+    setElementById('total', totalCost);
+})
--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 
 // Find Players Id
 
-function getPlayersNameById(selectPlayer){
+function getPlayersNameById(selectPlayer) {
     const playerName = document.getElementById(selectPlayer);
     const player = playerName.innerText;
     return player;
@@ -9,9 +9,36 @@ function getPlayersNameById(selectPlayer){
 
 // Set player in Order List
 
-function setPlayersById(setPlayer, selectPlayer){
+function setPlayersById(setPlayer, selectPlayer) {
     const allPlayers = document.getElementById(setPlayer);
     const playerList = document.createElement('li');
     const setList = playerList.innerText = selectPlayer;
+    
     allPlayers.appendChild(playerList);
+}
+
+// Input Field
+
+function getInputFieldById(inputElement) {
+    const inputField = document.getElementById(inputElement);
+    const inputFieldElementString = inputField.value;
+    const inputFieldElement = parseFloat(inputFieldElementString);
+    
+    return inputFieldElement;
+}
+
+// Find Expenses id (string to number)
+
+function getExpensesId(idName){
+    const playerCostElement = document.getElementById(idName);
+    const playerCostString = playerCostElement.innerText;
+    const playerCost = parseFloat(playerCostString);
+    return playerCost;
+}
+
+// Set expenses amount
+
+function setElementById(textId, inputId){
+    const setExpensesElement = document.getElementById(textId);
+    setExpensesElement.innerText = inputId;
 }
